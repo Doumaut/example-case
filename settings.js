e476c0d2b95c1954df06d4e152ca8cc3895cee84
Doumaut <108480751+Doumaut@users.noreply.github.com>
2023-11-09 23:50:08 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,18 +7,18 @@ global.APIs = {
 
 // Apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.my.id': 'Your Key',
+	'https://api.zeeoneofc.my.id': 'MSwlk5hpOuXSxp6',
 }
 
-global.namabot = "Bot Store"
-global.namaowner = "Zeeoneofc"
-global.packname = namabot
-global.author = namaowner
-global.myweb = "https://s.id/zeeoneofc"
-global.footer_text = "© " + namabot
+global.namabot = "PESURUH Bot"
+global.namaowner = "REW STORE"
+global.packname = PESURUH Bot
+global.author = REW STORE
+global.myweb = "https://linktr.ee/rewstoree"
+global.footer_text = "© " + PESURUH Bot
 global.pp_bot = fs.readFileSync("./image/foto.jpg")
-global.qris = fs.readFileSync("./image/qris.jpg")
-global.owner = ['62887435047326']
+global.qris = fs.readFileSync("./image/donasi.jpg")
+global.owner = ['6285732107960']
 global.pairingNumber = "" //pairing number to install bot 
 global.sessionName = 'session'
 global.background = "https://telegra.ph/file/40342e0085d521cb8b907.jpg" //maks size 50kb agar welcome nya gk lemot
